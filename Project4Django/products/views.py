314434c92bba6c8f2a6c1bca5229e579ef847e87
JoelWangu <95956284+JoeWangu@users.noreply.github.com>
2022-07-08 22:19:43 +0300

    ok

--- Project4Django/products/views.py
+++ Project4Django/products/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+
+
+def index(request):
+    return HttpResponse('hello world')
+
+
+def new(request):
+    return HttpResponse('new products')
+
 
 # Create your views here.
